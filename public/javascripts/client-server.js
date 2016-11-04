fc3891e08ec02b73f7c42b7c6fb0b1b580ba537c
Evan Simkowitz <esimkowitz@wustl.edu>
2016-11-03 21:54:01 -0500

    Optimizations for better client performance with more data series (tested up to 11). Significantly less CPU usage now. Updated the Flot and jQuery source files to the most recent.

--- public/javascripts/client-server.js
+++ public/javascripts/client-server.js
@@ -2,9 +2,8 @@
  * Starts the client and server pushing functionality
  */
 
-var totalPoints = 100;
+var totalPoints = 50;
 var serverUpdates = 100;
-var clientUpdates = 25;
 var socket, dataset;
 var now = new Date().getTime();
 var somePlot = null;
@@ -77,29 +76,80 @@ var options = {
             axisLabelFontSizePixels: 12,
             axisLabelFontFamily: 'Verdana, Arial',
             axisLabelPadding: 6
+        }, {
+            min: 0,
+            max: 100,
+            show: false,
+            position: "right",
+            axisLabelUseCanvas: true,
+            axisLabelFontSizePixels: 12,
+            axisLabelFontFamily: 'Verdana, Arial',
+            axisLabelPadding: 6
+        }, {
+            min: 0,
+            max: 100,
+            show: false,
+            position: "right",
+            axisLabelUseCanvas: true,
+            axisLabelFontSizePixels: 12,
+            axisLabelFontFamily: 'Verdana, Arial',
+            axisLabelPadding: 6
+        }, {
+            min: 0,
+            max: 100,
+            show: false,
+            position: "right",
+            axisLabelUseCanvas: true,
+            axisLabelFontSizePixels: 12,
+            axisLabelFontFamily: 'Verdana, Arial',
+            axisLabelPadding: 6
+        }, {
+            min: 0,
+            max: 100,
+            show: false,
+            position: "right",
+            axisLabelUseCanvas: true,
+            axisLabelFontSizePixels: 12,
+            axisLabelFontFamily: 'Verdana, Arial',
+            axisLabelPadding: 6
+        }, {
+            min: 0,
+            max: 100,
+            show: false,
+            position: "right",
+            axisLabelUseCanvas: true,
+            axisLabelFontSizePixels: 12,
+            axisLabelFontFamily: 'Verdana, Arial',
+            axisLabelPadding: 6
         }
     ]
 };
 
 function initData() {
-    var data1 = [], data2 = [], data3 = [], data4 = [], data5 = [], data6 = [];
+    var numSeries = 11;
+    var dataSeries = [];
+    for (var i = 0; i < numSeries; i++) {
+        dataSeries[i] = [];
+    }
     for (var i = 0; i < totalPoints; i++) {
-        var temp = [now += clientUpdates, 0];
+        var temp = [now += serverUpdates, 0];
  
-        data1.push(temp);
-        data2.push(temp);
-        data3.push(temp);
-        data4.push(temp);
-        data5.push(temp);
-        data6.push(temp);
+        for (var j = 0; j < numSeries; j++) {
+            dataSeries[j].push(temp);
+        }
     }
     dataset = [
-        { label: "data 1", data: data1, lines: { lineWidth: 1.2 }, color: "#00FF00", idx: 0},
-        { label: "data 2", data: data2, lines: { lineWidth: 1.2 }, color: "#0044FF", yaxis: 2, idx: 1 },
-        { label: "data 3", data: data3, lines: { lineWidth: 1.2 }, color: "#FF0000", yaxis: 3, idx: 2 },
-        { label: "data 4", data: data4, lines: { lineWidth: 1.2 }, color: "#551A8B", yaxis: 4, idx: 3 },
-        { label: "data 5", data: data5, lines: { lineWidth: 1.2 }, color: "#FFA500", yaxis: 5, idx: 4 },
-        { label: "data 6", data: data6, lines: { lineWidth: 1.2 }, color: "#FF69B4", yaxis: 6, idx: 5 }
+        { label: "data 1", data: dataSeries[0], lines: { lineWidth: 1.2 }, color: "#00FF00", idx: 0},
+        { label: "data 2", data: dataSeries[1], lines: { lineWidth: 1.2 }, color: "#0044FF", yaxis: 2, idx: 1 },
+        { label: "data 3", data: dataSeries[2], lines: { lineWidth: 1.2 }, color: "#FF0000", yaxis: 3, idx: 2 },
+        { label: "data 4", data: dataSeries[3], lines: { lineWidth: 1.2 }, color: "#551A8B", yaxis: 4, idx: 3 },
+        { label: "data 5", data: dataSeries[4], lines: { lineWidth: 1.2 }, color: "#FFA500", yaxis: 5, idx: 4 },
+        { label: "data 6", data: dataSeries[5], lines: { lineWidth: 1.2 }, color: "#FF69B4", yaxis: 6, idx: 5 },
+        { label: "data 7", data: dataSeries[6], lines: { lineWidth: 1.2 }, color: "#008080", yaxis: 7, idx: 6 },
+        { label: "data 8", data: dataSeries[7], lines: { lineWidth: 1.2 }, color: "#228B22", yaxis: 8, idx: 7 },
+        { label: "data 9", data: dataSeries[8], lines: { lineWidth: 1.2 }, color: "#104AAE", yaxis: 9, idx: 8 },
+        { label: "data 10", data: dataSeries[9], lines: { lineWidth: 1.2 }, color: "#AE10A7", yaxis: 10, idx: 9 },
+        { label: "data 11", data: dataSeries[10], lines: { lineWidth: 1.2 }, color: "#D8D019", yaxis: 11, idx: 10 }
     ];
     for (var i = 0; i < dataset.length; i++) {
         showData[i] = true;
@@ -116,7 +166,7 @@ var startClientServer = function() {
     socket = io.connect(host);
 
     socket.emit('updateInterval', serverUpdates);
-    /**
+    /*
      * Repaint graph function.  This repaints the graph
      * at a timed interval
      */
@@ -127,8 +177,10 @@ var startClientServer = function() {
             someData[i].data.shift();
             temp = [_data[0], _data[i+1]];
             someData[i].data.push(temp);
+            // someData[i].data = simplify(dataSeries[i], .08, true);
+            // console.log(dataSeries[i]);
+            // console.log(someData[i].data);
             someData[i].lines.show = showData[i];
-
         }
         somePlot.setData(someData);
         somePlot.setupGrid();
--- randomizer.js
+++ randomizer.js
@@ -2,7 +2,7 @@
  * Generates a random number or points to plot
  */
 var prev = [], coeff = [];
-var numSeries = 6;
+var numSeries = 11;
 for (var i = 0; i < numSeries; i++) {
     prev[i] = 50;
     coeff[i] = 1;
@@ -12,12 +12,12 @@ exports.getRandomData = function() {
     var y = [];
     y[0] = Date.now();
     for (var i = 0; i < numSeries; i++) {
-        y[i+1] = prev[i] + Math.random() * 5 - coeff[i] * Math.random() * 5;
+        y[i+1] = prev[i] + Math.random() * 2 - coeff[i] * Math.random() * 2;
         prev[i] = y[i+1];
         if (y[i+1] < 10) {
-            coeff[i] = -4;
+            coeff[i] = -2;
         } else if (y[i+1] > 90) {
-            coeff[i] = 4;
+            coeff[i] = 2;
         } else {
             coeff[i] = 1;
         }
