3243d1e7aeba17e8d9dad69ca833a01e818103a6
Henry Anyimadu <henry@whatsfabric.com>
2025-11-15 15:35:28 -0600

    updated licenscing

--- TelemetryWebApp/frontend-web-telemetry/src/components/plots/Lambda.vue
+++ TelemetryWebApp/frontend-web-telemetry/src/components/plots/Lambda.vue
@@ -44,8 +44,7 @@ const updateChart = () => {
 };
 
 onMounted(async () => {
-  SciChartSurface.useWasmFromCDN();
-  // SciChartSurface.setRuntimeLicenseKey("BDF7cXttucLnCaaYIAS54uSgBWzs487JMNwSckfExL1EP3xIsI4RElNwQLBZwn5trxtZ3IsQ8fRT5vz7K/eXslQKwlqyltc7sFCeuQJlbTngfgVUgFKKkL/ocK6+aE0zvnCLdBcmKp12xyCIMc/S25Nh50woR7K1ORF4wWXCBOGe6twlLm4UVBbsRrOpZI057eSH8jmk7GEM40WPQvGd+osy3cehnEezJ8QFg4TFYr67SRwAMDIRWh7z6XTWwS+ilrt8QTo1t6DDqXpQiz2lC/ajwAlxc90PXVKbXNAAxcvdPp4yIapUH/cMuoqNWL229ctFZc6wCe2+n0/deGgsYc8PXtHTzdrdcY+YP+TlQzwEKilPzLxtswLlPkggV9k+8sd4JiCy6Ok3WQ2nkbRaR0aUnZi3CEWMqHM7qORAxQsgcS6jwfc4zsmgnSq3zW4VRwU3DXf2BLAiW9/H3brDXEN+RaW8bJovXH2Y2gxmiMTN3t4Hlzjl9aFceNBRuehv1tuS7o4rQCmXX5YqUYJC7CWd8h4XtiW5n7q8vlxClLNH+4EYia2SvHLzhI/AWK/JzKpm7u1dNqltd117RowqORy9jeuxlUZRfeNUZfg98QcUZtnOK+OHsCnjH+r9FfJ5UcLge297/NkThKKuukUfWw==");
+  SciChartSurface.UseCommunityLicense();
 
   const { wasmContext, sciChartSurface: surface } = await SciChartSurface.create("scichart-lambda", {
     theme: new SciChartJSDarkTheme(),
