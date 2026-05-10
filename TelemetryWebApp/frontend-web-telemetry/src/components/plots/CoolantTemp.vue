333b9e142353481663ce92170876955124e42a24
Josh Kevil <jk15cohome@gmail.com>
2026-05-10 02:16:33 -0500

    Refactor coolant temperature chart update logic

--- TelemetryWebApp/frontend-web-telemetry/src/components/plots/CoolantTemp.vue
+++ TelemetryWebApp/frontend-web-telemetry/src/components/plots/CoolantTemp.vue
@@ -5,13 +5,15 @@
 </template>
 
 <script setup lang="ts">
-import {computed, onMounted, onUnmounted, watch} from "vue";
+import { computed, onMounted, onUnmounted, watch } from "vue";
 import { useFileStoreStore } from "../../stores/FileStore";
 import { chartSyncService } from "../../services/chartSync";
+import { channelConfig } from "../../services/channelConfig";
 import {
   SciChartSurface,
   NumericAxis,
   EAutoRange,
+  NumberRange,
   FastLineRenderableSeries,
   XyDataSeries,
   SciChartJSDarkTheme,
@@ -21,32 +23,26 @@ import {
   RolloverModifier
 } from "scichart";
 
-// Retrieve data from store
 const store = useFileStoreStore();
-let sciChartSurface: SciChartSurface | null = null; // To hold the chart surface instance
-let coolanttempDataSeries: XyDataSeries | null = null; // To hold the data series
+let sciChartSurface: SciChartSurface | null = null;
+let coolanttempDataSeries: XyDataSeries | null = null;
 
-const timeData = computed(() => store.Time);  // Make reactive
-const coolanttempData = computed(() => store.CoolantTemp); // Make reactive
-let liveStatus = store.isLive;
+const timeData = computed(() => store.Time);
+const coolanttempData = computed(() => store.CoolantTemp);
 
 const updateChart = () => {
-  if (!sciChartSurface || !coolanttempDataSeries) {
-    return; // Chart hasn't been initialized yet
-  }
-
-  if (liveStatus) {
-    let lb = coolanttempData.value.length - 1
-    let lt = timeData.value.length - 1
-    coolanttempDataSeries?.append(timeData.value[lt], coolanttempData.value[lb]);
-  } else {
-    coolanttempDataSeries?.clear();
-    coolanttempDataSeries?.appendRange(timeData.value, coolanttempData.value);
-  }
+  if (!sciChartSurface || !coolanttempDataSeries) return;
+  const cfg = channelConfig['CoolantTemp'];
+  coolanttempDataSeries.clear();
+  coolanttempDataSeries.appendRange(
+    timeData.value.slice(-cfg.windowSamples),
+    coolanttempData.value.slice(-cfg.windowSamples)
+  );
 };
 
 onMounted(async () => {
   SciChartSurface.UseCommunityLicense();
+  const cfg = channelConfig['CoolantTemp'];
 
   const { wasmContext, sciChartSurface: surface } = await SciChartSurface.create("scichart-coolanttemp", {
     theme: new SciChartJSDarkTheme(),
@@ -54,21 +50,22 @@ onMounted(async () => {
     titleStyle: { fontSize: 24 }
   });
   sciChartSurface = surface;
-
-
-  // Create the data series *once*
   coolanttempDataSeries = new XyDataSeries(wasmContext);
 
-  // ... (add axes, renderable series, etc., as before, but *outside* the watch)
-    // Renderable series
-    sciChartSurface.renderableSeries.add(new FastLineRenderableSeries(wasmContext, {
+  sciChartSurface.renderableSeries.add(new FastLineRenderableSeries(wasmContext, {
     stroke: "purple",
     strokeThickness: 3,
     opacity: 1,
     dataSeries: coolanttempDataSeries,
   }));
 
-    // Modifiers
+  sciChartSurface.xAxes.add(new NumericAxis(wasmContext, { autoRange: EAutoRange.Always, drawLabels: false }));
+  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, {
+    axisTitle: cfg.yUnit,
+    autoRange: EAutoRange.Never,
+    visibleRange: new NumberRange(cfg.yMin, cfg.yMax),
+  }));
+
   const modifierGroup = chartSyncService.modifierGroupId;
   sciChartSurface.chartModifiers.add(
     new ZoomPanModifier({ modifierGroup }),
@@ -77,19 +74,9 @@ onMounted(async () => {
     new RolloverModifier({ modifierGroup })
   );
 
-   // Axes
-  sciChartSurface.yAxes.add(new NumericAxis(wasmContext, { axisTitle: "C", autoRange: EAutoRange.Always}));
-  sciChartSurface.xAxes.add(new NumericAxis(wasmContext, { autoRange: EAutoRange.Always, drawLabels: false}));
-
   chartSyncService.register(sciChartSurface);
-
-
-  updateChart();// Initial chart setup
-
-  watch([timeData, coolanttempData], () => {
-    updateChart();  // Update the chart when data changes
-  });
-
+  updateChart();
+  watch([timeData, coolanttempData], updateChart);
 });
 
 onUnmounted(() => {
@@ -99,7 +86,6 @@ onUnmounted(() => {
     sciChartSurface = null;
   }
 });
-
 </script>
 
 <style scoped>
